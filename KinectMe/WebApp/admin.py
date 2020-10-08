99b8ed4adb9f6c7eaa8f42e88b27b5a4888dcadd
jeremyMarz <70720613+jeremymarzAtos@users.noreply.github.com>
2020-10-08 14:54:13 -0500

    model fixes

--- KinectMe/WebApp/admin.py
+++ KinectMe/WebApp/admin.py
@@ -5,3 +5,7 @@ from .forms import *
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Activities)
+admin.site.register(Event)
+
+
--- KinectMe/WebApp/models.py
+++ KinectMe/WebApp/models.py
@@ -1,62 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
-from djchoices import DjangoChoices, ChoiceItem
 
 # Create your models here.
-
-'''
-class Selection(models.Model):
+class Activities(models.Model):
     ACTIVITY= (
-        ("C", "Climbing"),
-        ("S", "Soccer"),
-        ("F", "Football"),
-        ("U", "Ultimate Frisbee"),
-        ("Y", "Yoga"),
-        ("R", "Running"),
-        ("B", "Basketball"),
-        ("V", "Vollyball"),
-        ("CY", "Cycling"),
-        ("G", "Golf"),
-        ("CO", "Cornhole"),
-        ("BA", "Baseball"),
-        ("SB", "Softball"),
-        ("Z", "Zumba"),
-        ("L", "Lifting"),
-        ("G", "Golf"),
-        ("CO", "Cornhole"),
-        ("BA", "Baseball"),
+        ("Climbing", "Climbing"),
+        ("Soccer", "Soccer"),
+        ("Football", "Football"),
+        ("Ultimate Frisbee", "Ultimate Frisbee"),
+        ("Yoga", "Yoga"),
+        ("Running", "Running"),
+        ("Basketball", "Basketball"),
+        ("Vollyball", "Vollyball"),
+        ("Cycling", "Cycling"),
+        ("Golf", "Golf"),
+        ("Cornhole", "Cornhole"),
+        ("Baseball", "Baseball"),
+        ("Softball", "Softball"),
+        ("Zumba", "Zumba"),
+        ("Lifing", "Lifting"),
+        ("Cricket", "Cricket"),
+        ("Tennis", "Tennis"),
+        ("Spikeball", "Spikeball"),
+        ("Other", "Other"),
 )
-    sport = models.CharField(max_length=15)
+    sport = models.CharField(max_length=20, unique=True, choices=ACTIVITY)
     def __str__(self):
         return self.sport
-    '''
+
 class Profile(models.Model):
-    #user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     user_name = models.CharField(max_length=20)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=30)
     email = models.CharField(max_length=40)
     age = models.CharField(max_length=2)
-    #tag1 = models.MultipleChoiceField(max_length=15, choices=ACTIVITIES)
+    sport = models.ManyToManyField(Activities)
 
-    class Selection(DjangoChoices):
-        Soccer = ChoiceItem("Soccer")
-        Vollyball = ChoiceItem("Vollyball")
-        Climbing = ChoiceItem("Climbing")
-        Football = ChoiceItem("Football")
-        Zumba = ChoiceItem("Zumba")
-        Yoga = ChoiceItem("Yoga")
-        Running = ChoiceItem("Running")
-        Golf = ChoiceItem("Golf")
-        Cornhole = ChoiceItem("Cornhole")
-        Ultimate = ChoiceItem("Ultimate")
-        Basketball = ChoiceItem("Basketball")
-        Softball = ChoiceItem("Softball")
-        Lifting = ChoiceItem("Lifting")
-
-    sport1 = models.CharField(max_length=20, choices=Selection.choices)
-    sport2 = models.CharField(max_length=20, choices=Selection.choices)
-    sport3 = models.CharField(max_length=20, choices=Selection.choices)
 
     def __str__(self):
         return self.user_name
+
+class Event(models.Model):
+    organizer = models.OneToOneField(Profile,on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=20)
+    sport = models.OneToOneField(Activities, on_delete=models.CASCADE, null=False)
