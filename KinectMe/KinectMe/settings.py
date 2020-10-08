d4c6c6ae8187d7ada367c2fccb80228677cd6cc0
chicho-terremoto <70280820+chicho-terremoto@users.noreply.github.com>
2020-10-08 17:10:21 -0500

    images working

--- KinectMe/KinectMe/settings.py
+++ KinectMe/KinectMe/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -120,3 +122,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+
+# TEMPLATE_DIRS = (
+#    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+#    # Always use forward slashes, even on Windows.
+#    # Don't forget to use absolute paths, not relative paths.
+#    BASE_DIR + '../WebApp/templates',
+# )
